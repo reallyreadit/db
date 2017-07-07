aac9b68b9febec2fb34b2ff2ed4a23af5882a549
Jeff Camera <jeff@jeffcamera.com>
2017-07-06 23:07:02 -0400

    fixed case-sensitive email matching

--- migration/0.15.sql
+++ migration/0.15.sql
@@ -1,3 +1,4 @@
+DROP FUNCTION user_account_api.find_user_account(email text);
 CREATE FUNCTION user_account_api.find_user_account(email text) RETURNS SETOF user_account_api.user_account
 LANGUAGE SQL AS $func$
 	SELECT * FROM user_account_api.user_account WHERE lower(email) = lower(find_user_account.email);

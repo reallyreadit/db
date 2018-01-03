ee942d4b9b1bb39babe971b68cc545ea9cc05920
Jeff Camera <jeff@jeffcamera.com>
2018-01-02 22:54:21 -0500

    removed old session functions

--- session-backup/get_session.sql
+++
@@ -1,4 +0,0 @@
-CREATE FUNCTION get_session(session_key	bytea) RETURNS SETOF session
-LANGUAGE SQL AS $func$
-	SELECT id, user_account_id, begin_date, end_date FROM session WHERE session.id = session_key;
-$func$;

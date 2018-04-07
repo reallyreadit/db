e34c6b25723162e22934eeb23ad03e6f1ecaaf09
jacamera <jeff@jeffcamera.com>
2018-04-07 10:28:50 -0400

    updated psql command formatting

--- create-database-objects.ps1
+++ create-database-objects.ps1
@@ -1,3 +1,3 @@
 foreach ($objectPath in (Get-Content .\database-objects)) {
-    psql --dbname=rrit --file=$objectPath --host=localhost --username=postgres
+    psql --host=localhost --username=postgres --dbname=rrit --file=$objectPath 
 }

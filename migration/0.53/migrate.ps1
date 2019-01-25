92ef804d968836d1ece53a344009d7f9926657e9
Jeff <jeff@jeffcamera.com>
2019-01-25 12:19:06 -0500

    fixed object file separator char

--- migration/0.53/migrate.ps1
+++ migration/0.53/migrate.ps1
@@ -16,7 +16,7 @@ $reverseObjects = [Array]::CreateInstance([object], $objects.Count)
 $objects.CopyTo($reverseObjects, 0)
 [Array]::Reverse($reverseObjects)
 foreach ($object in $reverseObjects) {
-    if ($object.Split([IO.Path]::DirectorySeparatorChar)[1].EndsWith('_api')) {
+    if ($object.Split('/')[1].EndsWith('_api')) {
         Write-Host $object
         & ./drop-database-object.ps1 -Hostname $Hostname -Username $Username -Database $database -Object $object
     }
@@ -35,7 +35,7 @@ Write-Host 'Creating api objects...'
 Push-Location ./next
 
 foreach ($object in Get-Content ./database-objects) {
-    if ($object.Split([IO.Path]::DirectorySeparatorChar)[1].EndsWith('_api')) {
+    if ($object.Split('/')[1].EndsWith('_api')) {
         psql --host=$Hostname --username=$Username --dbname=$database --file=$object
     }
 }

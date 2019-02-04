9c8dd0b90a658913190a83752b0868aa593afe93
jeff <jeff@jeffcamera.com>
2019-02-04 12:54:40 -0500

    added rating table

---
+++ migration/0.54/migrate-core.sql
@@ -0,0 +1,7 @@
+CREATE TABLE rating (
+   id bigserial PRIMARY KEY,
+   timestamp timestamp NOT NULL DEFAULT utc_now(),
+   score int NOT NULL,
+   article_id bigint NOT NULL REFERENCES article (id),
+   user_account_id bigint NOT NULL REFERENCES user_account (id)
+);
---
+++ schemas/article_api/functions/rate_article.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION article_api.rate_article(
+	article_id bigint,
+	user_account_id bigint,
+	score int
+)
+RETURNS SETOF rating
+LANGUAGE SQL
+STRICT
+AS $$
+	INSERT INTO rating (score, article_id, user_account_id)
+	VALUES (score, article_id, user_account_id)
+	RETURNING *;
+$$;

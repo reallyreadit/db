53d0eccf653e7af8762ce56fc7b629c9173983fd
Jeff <jeff@jeffcamera.com>
2021-02-15 17:59:21 -0500

    added subscription creation dev scripts

---
+++ dev-scripts/create-apple-subscription.sql
@@ -0,0 +1,60 @@
+\echo 'Creating subscription account...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_or_update_subscription_account(
+		provider := 'apple',
+		provider_account_id := 'test_cus_' || :id,
+		user_account_id := (
+			SELECT
+				user_account.id
+			FROM
+				core.user_account
+			WHERE
+				user_account.name = :user
+		),
+		date_created := :begin
+	);
+
+\echo 'Creating subscription...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_or_update_subscription(
+		provider := 'apple',
+		provider_subscription_id := 'test_sub_' || :id,
+		provider_account_id := 'test_cus_' || :id,
+		date_created := :begin,
+		date_terminated := NULL,
+		latest_receipt := '0'
+	);
+
+\echo 'Creating subscription period...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_or_update_subscription_period(
+		provider := 'apple',
+		provider_period_id := 'test_in_' || :id,
+		provider_subscription_id := 'test_sub_' || :id,
+		provider_price_id := (
+			SELECT
+				price_level.provider_price_id
+			FROM
+				subscriptions.price_level
+			WHERE
+				price_level.provider = 'apple' AND
+				price_level.name = :level
+		),
+		provider_payment_method_id := NULL,
+		begin_date := :begin,
+		end_date := :end,
+		date_created := :begin,
+		payment_status := 'succeeded',
+		date_paid := :begin,
+		date_refunded := NULL,
+		refund_reason := NULL
+	);
---
+++ dev-scripts/create-stripe-custom-subscription.sql
@@ -0,0 +1,93 @@
+\echo 'Creating subscription account...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_or_update_subscription_account(
+		provider := 'stripe',
+		provider_account_id := 'test_cus_' || :id,
+		user_account_id := (
+			SELECT
+				user_account.id
+			FROM
+				core.user_account
+			WHERE
+				user_account.name = :user
+		),
+		date_created := :begin
+	);
+
+\echo 'Creating payment method...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_payment_method(
+		provider := 'stripe',
+		provider_payment_method_id := 'test_pm_' || :id,
+		provider_account_id := 'test_cus_' || :id,
+		date_created := :begin,
+		wallet := 'none',
+		brand := :card_brand,
+		last_four_digits := :card_digits,
+		country := :card_country,
+		expiration_month := :card_exp_month,
+		expiration_year := :card_exp_year
+	);
+
+\echo 'Assigning default payment method...'
+
+SELECT
+	*
+FROM
+	subscriptions.assign_default_payment_method(
+   	provider := 'stripe',
+   	provider_account_id := 'test_cus_' || :id,
+   	provider_payment_method_id := 'test_pm_' || :id
+	);
+
+\echo 'Creating custom price...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_custom_price(
+		provider := 'stripe',
+		provider_price_id := 'test_price_' || :id,
+		date_created := :begin,
+		amount := :price
+	);
+
+\echo 'Creating subscription...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_or_update_subscription(
+		provider := 'stripe',
+		provider_subscription_id := 'test_sub_' || :id,
+		provider_account_id := 'test_cus_' || :id,
+		date_created := :begin,
+		date_terminated := NULL,
+		latest_receipt := NULL
+	);
+
+\echo 'Creating subscription period...'
+
+SELECT
+	*
+FROM
+	subscriptions.create_or_update_subscription_period(
+		provider := 'stripe',
+		provider_period_id := 'test_in_' || :id,
+		provider_subscription_id := 'test_sub_' || :id,
+		provider_price_id := 'test_price_' || :id,
+		provider_payment_method_id := 'test_pm_' || :id,
+		begin_date := :begin,
+		end_date := :end,
+		date_created := :begin,
+		payment_status := 'succeeded',
+		date_paid := :begin,
+		date_refunded := NULL,
+		refund_reason := NULL
+	);

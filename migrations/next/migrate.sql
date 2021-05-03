dad610962a47eb0135ebee198857503d14905628
Jeff <jeff@jeffcamera.com>
2021-05-03 12:31:19 -0400

    added live product seed data to migration script

--- migrations/next/migrate.sql
+++ migrations/next/migrate.sql
@@ -2774,3 +2774,72 @@ AS $$
 	FROM
 		allocation;
 $$;
+
+-- seed product data
+INSERT INTO
+	core.subscription_level (
+		id,
+		name,
+		amount
+	)
+VALUES
+	(
+		1,
+		'Budget',
+		499
+	),
+	(
+		2,
+		'Reader',
+		1499
+	),
+	(
+		3,
+		'Super Reader',
+		2499
+	);
+
+INSERT INTO
+   core.subscription_price (
+   	provider,
+   	provider_price_id,
+   	date_created,
+   	level_id
+   )
+VALUES
+	(
+		'apple',
+		'v1_level1',
+		core.utc_now(),
+		1
+	),
+   (
+		'apple',
+		'v1_level2',
+		core.utc_now(),
+		2
+	),
+   (
+		'apple',
+		'v1_level3',
+		core.utc_now(),
+		3
+	),
+   (
+		'stripe',
+		'price_1In4cdCE1GzMLmqKRCTbqTGR',
+		core.utc_now(),
+		1
+	),
+   (
+		'stripe',
+		'price_1In4cdCE1GzMLmqK1Dmh9hKF',
+		core.utc_now(),
+		2
+	),
+   (
+		'stripe',
+		'price_1In4cdCE1GzMLmqKQxqbyI2g',
+		core.utc_now(),
+		3
+	);

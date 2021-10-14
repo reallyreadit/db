389e1127d931dcf9b88e5da455052bc5fb74170e
Jeff <jeff@jeffcamera.com>
2021-10-13 22:33:20 -0400

    updated stripe price_id dev script

--- dev-scripts/update-stripe-price-ids.sql
+++ dev-scripts/update-stripe-price-ids.sql
@@ -1,22 +1,47 @@
 -- set up for stripe subscription testing
-UPDATE
-	core.subscription_price
-SET
-	provider_price_id = ''
-WHERE
-	provider = 'stripe'::core.subscription_provider AND
-	level_id = 1;
-UPDATE
-	core.subscription_price
-SET
-	provider_price_id = ''
-WHERE
-	provider = 'stripe'::core.subscription_provider AND
-	level_id = 2;
-UPDATE
-	core.subscription_price
-SET
-	provider_price_id = ''
-WHERE
-	provider = 'stripe'::core.subscription_provider AND
-	level_id = 3;
+CREATE TEMP TABLE
+	price_id_migration (
+		prod_id,
+		dev_id
+	) AS
+VALUES
+	('price_1In4cdCE1GzMLmqKRCTbqTGR', 'price_1In6LdCE1GzMLmqKo9JXXYpK'),
+	('price_1In4cdCE1GzMLmqK1Dmh9hKF', 'price_1In6LdCE1GzMLmqKviYeislC'),
+	('price_1In4cdCE1GzMLmqKQxqbyI2g', 'price_1In6LdCE1GzMLmqK73XjFH1Z');
+
+WITH updated_subscription_price AS (
+	UPDATE
+		core.subscription_price
+	SET
+		provider_price_id = price_id_migration.dev_id
+	FROM
+		price_id_migration
+	WHERE
+		provider_price_id = price_id_migration.prod_id
+	RETURNING
+		*
+),
+updated_subscription_period AS (
+	UPDATE
+		core.subscription_period
+	SET
+		provider_price_id = price_id_migration.dev_id
+	FROM
+		price_id_migration
+	WHERE
+		provider_price_id = price_id_migration.prod_id
+),
+updated_subscription_renewal_status_change AS (
+	UPDATE
+		core.subscription_renewal_status_change
+	SET
+		provider_price_id = price_id_migration.dev_id
+	FROM
+		price_id_migration
+	WHERE
+		provider_price_id = price_id_migration.prod_id
+)
+SELECT
+	*
+FROM
+	updated_subscription_price;

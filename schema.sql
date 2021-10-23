6d11bcff5e8fe63a9151d3381aeae18341aaab02
Jeff <jeff@jeffcamera.com>
2021-10-23 12:35:36 -0400

    added user date created email filters

--- schema.sql
+++ schema.sql
@@ -6307,6 +6307,166 @@ $$;
 
 
 --
+-- Name: create_company_update_notifications(bigint, text, text, text, boolean, timestamp without time zone, timestamp without time zone); Type: FUNCTION; Schema: notifications; Owner: -
+--
+
+CREATE FUNCTION notifications.create_company_update_notifications(author_id bigint, subject text, body text, subscription_status_filter text, free_for_life_filter boolean, user_created_after_filter timestamp without time zone, user_created_before_filter timestamp without time zone) RETURNS SETOF notifications.email_dispatch
+    LANGUAGE sql
+    AS $$
+	WITH recipient AS (
+		SELECT
+			user_account.id AS user_account_id
+		FROM
+			core.user_account
+			JOIN
+				notifications.current_preference ON
+					user_account.id = current_preference.user_account_id
+			LEFT JOIN
+				core.subscription_account ON
+					user_account.id = subscription_account.user_account_id
+			LEFT JOIN
+				core.subscription ON
+					subscription_account.provider = subscription.provider AND
+					subscription_account.provider_account_id = subscription.provider_account_id
+			LEFT JOIN
+				core.subscription_period ON
+					subscription.provider = subscription_period.provider AND
+					subscription.provider_subscription_id = subscription_period.provider_subscription_id
+		WHERE
+			current_preference.company_update_via_email AND
+			CASE
+				create_company_update_notifications.subscription_status_filter::notifications.bulk_email_subscription_status_filter
+			WHEN
+				'currently_subscribed'::notifications.bulk_email_subscription_status_filter
+			THEN
+				subscriptions.is_user_subscribed(
+					user_account := user_account,
+					as_of_date := core.utc_now()
+				)
+			WHEN
+				'not_currently_subscribed'::notifications.bulk_email_subscription_status_filter
+			THEN
+				NOT subscriptions.is_user_subscribed(
+					user_account := user_account,
+					as_of_date := core.utc_now()
+				)
+			ELSE
+				TRUE
+			END AND
+			CASE
+				create_company_update_notifications.free_for_life_filter
+			WHEN
+				TRUE
+			THEN
+				subscriptions.is_user_free_for_life(
+					user_account := user_account
+				)
+			WHEN
+				FALSE
+			THEN
+				NOT subscriptions.is_user_free_for_life(
+					user_account := user_account
+				)
+			ELSE
+				TRUE
+			END AND
+			CASE WHEN
+				create_company_update_notifications.user_created_after_filter IS NOT NULL
+			THEN
+				user_account.date_created >= create_company_update_notifications.user_created_after_filter
+			ELSE
+				TRUE
+			END AND
+			CASE WHEN
+				create_company_update_notifications.user_created_before_filter IS NOT NULL
+			THEN
+				user_account.date_created < create_company_update_notifications.user_created_before_filter
+			ELSE
+				TRUE
+			END
+		GROUP BY
+			user_account.id
+		HAVING
+			CASE
+				create_company_update_notifications.subscription_status_filter::notifications.bulk_email_subscription_status_filter
+			WHEN
+				'never_subscribed'::notifications.bulk_email_subscription_status_filter
+			THEN
+				every(subscription_period.payment_status IS DISTINCT FROM 'succeeded'::core.subscription_payment_status)
+			ELSE
+				TRUE
+			END
+	),
+	update_event AS (
+		INSERT INTO
+			core.notification_event (
+				type,
+				bulk_email_author_id,
+				bulk_email_subject,
+				bulk_email_body,
+				bulk_email_subscription_status_filter,
+				bulk_email_free_for_life_filter,
+				bulk_email_user_created_after_filter,
+				bulk_email_user_created_before_filter
+			)
+		SELECT
+			'company_update',
+			create_company_update_notifications.author_id,
+			create_company_update_notifications.subject,
+			create_company_update_notifications.body,
+			create_company_update_notifications.subscription_status_filter::notifications.bulk_email_subscription_status_filter,
+			create_company_update_notifications.free_for_life_filter,
+			create_company_update_notifications.user_created_after_filter,
+			create_company_update_notifications.user_created_before_filter
+		FROM
+			recipient
+		LIMIT
+			1
+		RETURNING
+			id
+	),
+	receipt AS (
+		INSERT INTO
+			core.notification_receipt (
+				event_id,
+				user_account_id,
+				via_email,
+				via_extension,
+				via_push,
+				event_type
+			)
+		SELECT
+			(
+				SELECT
+					update_event.id
+				FROM
+					update_event
+			),
+			recipient.user_account_id,
+			TRUE,
+			FALSE,
+			FALSE,
+			'company_update'::core.notification_event_type
+		FROM
+			recipient
+		RETURNING
+			id,
+			user_account_id
+	)
+	SELECT
+		receipt.id,
+		user_account.id,
+		user_account.name,
+		user_account.email
+	FROM
+		receipt
+		JOIN
+			core.user_account ON
+				user_account.id = receipt.user_account_id;
+$$;
+
+
+--
 -- Name: create_email_notification(text, text, text, text); Type: FUNCTION; Schema: notifications; Owner: -
 --
 
@@ -7596,7 +7756,7 @@ $$;
 -- Name: get_bulk_mailings(); Type: FUNCTION; Schema: notifications; Owner: -
 --
 
-CREATE FUNCTION notifications.get_bulk_mailings() RETURNS TABLE(id bigint, date_sent timestamp without time zone, subject text, body text, type core.notification_event_type, subscription_status_filter notifications.bulk_email_subscription_status_filter, free_for_life_filter boolean, user_account text, recipient_count bigint)
+CREATE FUNCTION notifications.get_bulk_mailings() RETURNS TABLE(id bigint, date_sent timestamp without time zone, subject text, body text, type core.notification_event_type, subscription_status_filter notifications.bulk_email_subscription_status_filter, free_for_life_filter boolean, user_created_after_filter timestamp without time zone, user_created_before_filter timestamp without time zone, user_account text, recipient_count bigint)
     LANGUAGE sql
     AS $$
 	SELECT
@@ -7607,6 +7767,8 @@ CREATE FUNCTION notifications.get_bulk_mailings() RETURNS TABLE(id bigint, date_
 		event.type,
 		event.bulk_email_subscription_status_filter,
 		event.bulk_email_free_for_life_filter,
+		event.bulk_email_user_created_after_filter,
+		event.bulk_email_user_created_before_filter,
 		user_account.name AS user_account,
 		count(*) AS recipient_count
 	FROM
@@ -14766,7 +14928,9 @@ CREATE TABLE core.notification_event (
     bulk_email_subject text,
     bulk_email_body text,
     bulk_email_subscription_status_filter notifications.bulk_email_subscription_status_filter,
-    bulk_email_free_for_life_filter boolean
+    bulk_email_free_for_life_filter boolean,
+    bulk_email_user_created_after_filter timestamp without time zone,
+    bulk_email_user_created_before_filter timestamp without time zone
 );
 
 
